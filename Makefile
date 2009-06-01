92b8ed451a19225230f158ea765e710674e94c79
Francesc Vila <francesc@beamish.localdomain>
2009-06-01 18:59:21 +0200

    Ara si

--- Makefile
+++ Makefile
@@ -8,18 +8,16 @@ QSF=$(GRLIB)/boards/$(BOARD)/$(TOP).qsf
 EFFORT=std
 XSTOPT=
 SYNPOPT="set_option -pipe 0; set_option -retiming 0; set_option -write_apr_constraint 0"
-VHDLSYNFILES=config.vhd ahbrom.vhd ni/lib.vhd ni/ni_state_slv.vhd ni/packetizer.vhd ni/type_gen.vhd ni/counter.vhd ni/wormhole_splitter.vhd ni/wormhole_joiner.vhd
-
-#ni/ni.vhd router/port_buffer.vhd #leon3mp.vhd
+VHDLSYNFILES=config.vhd ahbrom.vhd ni/lib.vhd ni/ni_state_slv.vhd ni/packetizer.vhd ni/type_gen.vhd ni/counter.vhd ni/wormhole_splitter.vhd ni/wormhole_joiner.vhd ni/ni.vhd router/port_buffer.vhd #leon3mp.vhd
 
 #type_gen.vhd 
 
 # ni.vhd leon3mp.vhd
 
-#VHDLSIMFILES=testbench.vhd
-#SIMTOP=testbench
-VHDLSIMFILES=testbench_wormhole2.vhd
-SIMTOP=testbench_wormhole
+VHDLSIMFILES=testbench_test.vhd
+SIMTOP=testbench_test
+#VHDLSIMFILES=testbench_ni.vhd
+#SIMTOP=testbench_ni
 SDCFILE=$(GRLIB)/boards/$(BOARD)/default.sdc
 BITGEN=$(GRLIB)/boards/$(BOARD)/default.ut
 CLEAN=soft-clean
--- config.vhd
+++ config.vhd
@@ -45,38 +45,38 @@ package config is
   constant CFG_SVT : integer := 0;
   constant CFG_RSTADDR : integer := 16#00000#;
   constant CFG_LDDEL : integer := (1);
-  constant CFG_NWP : integer := (0);
-  constant CFG_PWD : integer := 0*2;
+  constant CFG_NWP : integer := (2);
+  constant CFG_PWD : integer := 1*2;
   constant CFG_FPU : integer := 0 + 16*0;
   constant CFG_GRFPUSH : integer := 0;
   constant CFG_ICEN : integer := 1;
-  constant CFG_ISETS : integer := 1;
-  constant CFG_ISETSZ : integer := 4;
+  constant CFG_ISETS : integer := 2;
+  constant CFG_ISETSZ : integer := 2;
   constant CFG_ILINE : integer := 8;
-  constant CFG_IREPL : integer := 0;
+  constant CFG_IREPL : integer := 1;
   constant CFG_ILOCK : integer := 0;
   constant CFG_ILRAMEN : integer := 0;
   constant CFG_ILRAMADDR: integer := 16#8E#;
   constant CFG_ILRAMSZ : integer := 1;
   constant CFG_DCEN : integer := 1;
-  constant CFG_DSETS : integer := 1;
-  constant CFG_DSETSZ : integer := 4;
+  constant CFG_DSETS : integer := 2;
+  constant CFG_DSETSZ : integer := 2;
   constant CFG_DLINE : integer := 8;
-  constant CFG_DREPL : integer := 0;
+  constant CFG_DREPL : integer := 1;
   constant CFG_DLOCK : integer := 0;
   constant CFG_DSNOOP : integer := 0 + 0 + 4*0;
   constant CFG_DFIXED : integer := 16#0#;
   constant CFG_DLRAMEN : integer := 0;
   constant CFG_DLRAMADDR: integer := 16#8F#;
   constant CFG_DLRAMSZ : integer := 1;
-  constant CFG_MMUEN : integer := 1;
-  constant CFG_ITLBNUM : integer := 8;
-  constant CFG_DTLBNUM : integer := 8;
-  constant CFG_TLB_TYPE : integer := 0 + 0*2;
+  constant CFG_MMUEN : integer := 0;
+  constant CFG_ITLBNUM : integer := 2;
+  constant CFG_DTLBNUM : integer := 2;
+  constant CFG_TLB_TYPE : integer := 1 + 0*2;
   constant CFG_TLB_REP : integer := 1;
-  constant CFG_DSU : integer := 0;
-  constant CFG_ITBSZ : integer := 0;
-  constant CFG_ATBSZ : integer := 0;
+  constant CFG_DSU : integer := 1;
+  constant CFG_ITBSZ : integer := 1;
+  constant CFG_ATBSZ : integer := 1;
   constant CFG_LEON3FT_EN : integer := 0;
   constant CFG_IUFT_EN : integer := 0;
   constant CFG_FPUFT_EN : integer := 0;
@@ -121,11 +121,11 @@ package config is
   constant CFG_MCTRL_RAM8BIT : integer := 0;
   constant CFG_MCTRL_RAM16BIT : integer := 0;
   constant CFG_MCTRL_5CS : integer := 0;
-  constant CFG_MCTRL_SDEN : integer := 1;
+  constant CFG_MCTRL_SDEN : integer := 0;
   constant CFG_MCTRL_SEPBUS : integer := 0;
   constant CFG_MCTRL_INVCLK : integer := 0;
   constant CFG_MCTRL_SD64 : integer := 0;
-  constant CFG_MCTRL_PAGE : integer := 1 + 0;
+  constant CFG_MCTRL_PAGE : integer := 0 + 0;
 -- SDRAM controller
   constant CFG_SDCTRL : integer := 0;
   constant CFG_SDCTRL_INVCLK : integer := 0;
@@ -180,8 +180,8 @@ package config is
   constant CFG_UART1_ENABLE : integer := 1;
   constant CFG_UART1_FIFO : integer := 4;
 -- UART 2
-  constant CFG_UART2_ENABLE : integer := 0;
-  constant CFG_UART2_FIFO : integer := 1;
+  constant CFG_UART2_ENABLE : integer := 1;
+  constant CFG_UART2_FIFO : integer := 4;
 -- LEON3 interrupt controller
   constant CFG_IRQ3_ENABLE : integer := 1;
 -- Modular timer
@@ -191,13 +191,13 @@ package config is
   constant CFG_GPT_TW : integer := (32);
   constant CFG_GPT_IRQ : integer := (8);
   constant CFG_GPT_SEPIRQ : integer := 1;
-  constant CFG_GPT_WDOGEN : integer := 1;
-  constant CFG_GPT_WDOG : integer := 16#FFFF#;
+  constant CFG_GPT_WDOGEN : integer := 0;
+  constant CFG_GPT_WDOG : integer := 16#0#;
 -- GPIO port
-  constant CFG_GRGPIO_ENABLE : integer := 1;
+  constant CFG_GRGPIO_ENABLE : integer := 0;
   constant CFG_GRGPIO_IMASK : integer := 16#0000#;
-  constant CFG_GRGPIO_WIDTH : integer := (8);
+  constant CFG_GRGPIO_WIDTH : integer := 1;
 
 -- GRLIB debugging
-  constant CFG_DUART : integer := 0;
+  constant CFG_DUART : integer := 1;
 end;
